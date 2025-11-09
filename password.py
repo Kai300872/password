b5daa9eed561c4721d1656c6ca9ec3e0eb952ab1
Kai <den71997@gmail.com>
2025-11-09 22:59:06 +0300

    Добавил функционал

--- password.py
+++ password.py
@@ -1,1 +1,20 @@
-dfbmdbmdfokbmdfkbmkdfbmkdfmbkdfbm
+import random   # для случайных чисел
+import string   # содержит буквы, цифры и символы
+
+# --- просим пользователя ввести длину пароля ---
+length = input("Введите длину пароля: ")
+
+# проверяем, что пользователь ввёл число
+if not length.isdigit():
+    print("Ошибка: нужно ввести число!")
+else:
+    length = int(length)  # превращаем строку в число
+
+    # набор символов: буквы, цифры, спецсимволы
+    all_chars = string.ascii_letters + string.digits + string.punctuation
+
+    # создаём пароль нужной длины
+    password = ''.join(random.choice(all_chars) for _ in range(length))
+
+    # выводим результат
+    print("Ваш пароль:", password)
